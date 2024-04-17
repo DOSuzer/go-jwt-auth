3aaa8deca6297e94fab65d1b592abe57d1e1e0bd
Егор Попов <egor.popov@arinteg.ru>
2024-04-17 17:15:03 +0300

    Minor changes

--- models/User.go
+++ models/User.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Email    string `gorm:"unique" json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name,omitempty"`
+	Email    string `gorm:"unique" json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 	Role     string `json:"role"`
 }
 
 type UserNameChange struct {
-	Name string `json:"name"`
+	Name string `json:"name" binding:"required"`
 }
